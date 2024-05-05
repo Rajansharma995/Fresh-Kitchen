59e471a49c72fcfd6c316a2af2d44f291930433b
Rajansharma995 <rajan.kandel.662@gmail.com>
2024-05-05 23:06:47 +0545

    pages

---
+++ src/Components/Header/Food.Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Food.Header.css";
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order Your Favourite Food Here</h2>
+            <p>Choose From The Diversese Menu featuring the tasty and healty food.Our Mission is to satisfy your taste with kitchen in your Device</p>
+            <button>View-Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
